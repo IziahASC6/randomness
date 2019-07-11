4359b2609485a82df8d48807bdc309e6cc717a86
IziahASC6 <iziahhouse@gmail.com>
2019-07-11 11:57:03 -0400

    Randomness

---
+++ randomness.js
@@ -0,0 +1,42 @@
+// GLOBAL VARIABLES //
+/////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const foods = [
+"pizza",
+"burgers",
+"rice and beans",
+"lasagna",
+"macaroni and cheese",
+"baked potatoes"
+];
+const students = [
+"Bob",
+"Martin",
+"Danny",
+"Madhi",
+"Harry",
+"Sean",
+"Robert"
+];
+//const decimal = Math.random();
+//console.log(decimal);
+//const times21 = decimal * 21;
+//console.log(times21);
+//const final = Math.floor(times21);
+//console.log(final);
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+console.log(alphabet)
+
+function randomNumber() {
+    const decimal = Math.random();
+console.log(decimal);
+const times26 = decimal * 26;
+console.log(times26);
+const final = Math.floor(times26);
+console.log(final);
+}
+
+randomNumber();
+
+
